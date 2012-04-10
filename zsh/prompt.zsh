fb8727f16fd144b8738088d66f2c11eb57f02bda
Matt Rohrer <matt@prognostikos.com>
2012-04-10 20:39:08 +0200

    redo prompt in sh for speed

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,4 +3,4 @@ local smiley="%(?,%{$fg[cyan]%}☺%{$reset_color%},%{$fg[magenta]%}☹%{$reset_c
 PROMPT='%{$fg[cyan]%}%~ %{$reset_color%}
 ${smiley} %{$reset_color%}'
 
-RPROMPT='%{$fg[magenta]%} $(rbenv version-name)$(git-cwd-info.rb)%{$reset_color%}'
+RPROMPT=' %{$fg[magenta]%}$(rbenv version-name)%{$reset_color%} %{$fg[blue]%}$(git-cwd-info)%{$reset_color%}'
