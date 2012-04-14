c14bf933771f1e0ff50a5f3726bb07e042345ad8
Matt Rohrer <matt@prognostikos.com>
2012-04-14 22:52:33 +0200

    add ability to switch back to informative prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,7 +2,10 @@ local smiley="%(?,%{$fg[cyan]%}☺%{$reset_color%},%{$fg[magenta]%}☹%{$reset_c
 
 PROMPT='%{$fg[cyan]%}%~ %{$reset_color%}
 ${smiley} %{$reset_color%}'
+fancy_left=$PROMPT
 
 RPROMPT=' %{$fg[magenta]%}$(rbenv version-name)%{$reset_color%} %{$fg[blue]%}$(git-cwd-info)%{$reset_color%}'
+fancy_right=$RPROMPT
 
+fancy_prompt(){ export PROMPT=$fancy_left; export RPROMPT=$fancy_right}
 simple_prompt(){ export PROMPT='$ '; export RPROMPT='' }
