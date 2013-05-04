8da0e16614cb338f34d7903345a4691f7ab0676b
Matt Rohrer <matt@prognostikos.com>
2013-05-04 07:17:53 +0200

    Update pretty_git_log and switch to short status

--- git/git.zsh
+++ git/git.zsh
@@ -21,21 +21,26 @@ SUBJECT="%s"
 
 FORMAT="$HASH}$RELATIVE_TIME}$AUTHOR}$REFS $SUBJECT"
 
+ANSI_RED='\033[31m'
+ANSI_RESET='\033[0m'
+
 show_git_head() {
     pretty_git_log -1
     git show -p --pretty="tformat:"
 }
 
 pretty_git_log() {
-    git log --graph --abbrev-commit --date=relative --pretty="tformat:${FORMAT}" $* |
+    git log --graph --pretty="tformat:${FORMAT}" $* |
         # Replace (2 years ago) with (2 years)
-        sed -Ee 's/(^[^<]*) ago)/\1)/' |
+        sed -Ee 's/(^[^<]*) ago\)/\1)/' |
         # Replace (2 years, 5 months) with (2 years)
-        sed -Ee 's/(^[^<]*), [[:digit:]]+ .*months?)/\1)/' |
+        sed -Ee 's/(^[^<]*), [[:digit:]]+ .*months?\)/\1)/' |
         # Line columns up based on } delimiter
         column -s '}' -t |
+        # Color merge commits specially
+        sed -Ee "s/(Merge branch .* into .*$)/$(printf $ANSI_RED)\1$(printf $ANSI_RESET)/" |
         # Page only if we need to
-        less -FRS
+        less -FXRS
 }
 
 # don't use git ls-files when tab-completing in a git repo
