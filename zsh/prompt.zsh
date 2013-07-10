2d8562514f5def426d98038f29e95e6c3c359927
Matt Rohrer <matt@prognostikos.com>
2013-07-10 08:25:11 +0200

    re-add ruby version to RPROMPT

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -6,9 +6,10 @@ local username=''
 [ 'rohrer' != $USER ] && username="%n@"
 
 PROMPT='%{$fg[magenta]%}${username}${hostname}%{$fg[blue]%}%.%{$fg[cyan]%}$(git-cwd-info)%{$reset_color%}%# '
+RPROMPT='%{$fg[white]%} $(rbenv version-name)%{$reset_color%}'
 
 local fancy_left=$PROMPT
-local fancy_right=''
+local fancy_right=$RPROMPT
 
 fancy_prompt(){ export PROMPT=$fancy_left; export RPROMPT=$fancy_right}
 simple_prompt(){ export PROMPT='$ '; export RPROMPT='' }
