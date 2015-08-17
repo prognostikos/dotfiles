235cda10692ce351a7641f7df6802428ac447775
Matt Rohrer <matt@prognostikos.com>
2015-08-17 08:07:11 +0200

    Always let me choose a hidden dir

--- fzf/commands.zsh
+++ fzf/commands.zsh
@@ -29,14 +29,6 @@ fo() {
 # fd - cd to selected directory
 fd() {
   local dir
-  dir=$(find ${1:-*} -path '*/\.*' -prune \
-                  -o -type d -print 2> /dev/null | fzf +m) &&
-  cd "$dir"
-}
-
-# fda - including hidden directories
-fda() {
-  local dir
   dir=$(find ${1:-.} -type d 2> /dev/null | fzf +m) && cd "$dir"
 }
 
