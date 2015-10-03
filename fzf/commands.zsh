7b038c2da80b8de813c37ce44a9207f6c9498e3e
Matt Rohrer <matt@prognostikos.com>
2015-10-03 10:57:37 +0200

    v should be fe

--- fzf/commands.zsh
+++ fzf/commands.zsh
@@ -195,12 +195,16 @@ ftpane() {
 }
 
 # v - open files in ~/.viminfo
+# v() {
+#   local files
+#   files=$(grep '^>' ~/.viminfo | cut -c3- |
+#           while read line; do
+#             [ -f "${line/\~/$HOME}" ] && echo "$line"
+#           done | fzf-tmux -d -m -q "$*" -1) && vim ${files//\~/$HOME}
+# }
+
 v() {
-  local files
-  files=$(grep '^>' ~/.viminfo | cut -c3- |
-          while read line; do
-            [ -f "${line/\~/$HOME}" ] && echo "$line"
-          done | fzf-tmux -d -m -q "$*" -1) && vim ${files//\~/$HOME}
+  fe
 }
 
 # c - browse chrome history
