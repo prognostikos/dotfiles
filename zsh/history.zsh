618ccacc6922e06d97ca488551bd5a3aa2374cf5
Matt Rohrer <matt@prognostikos.com>
2022-10-18 09:25:09 +0200

    Save more history

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -1,6 +1,8 @@
+alias history="history 1"
 HISTFILE=$HOME/.zhistory       # enable history saving on shell exit
-HISTSIZE=1200                  # lines of history to maintain memory
-SAVEHIST=1000                  # lines of history to maintain in history file.
+HISTSIZE=99999                 # lines of history to maintain memory
+SAVEHIST=99999                 # lines of history to maintain in history file.
+HISTFILESIZE=99999
 setopt HIST_EXPIRE_DUPS_FIRST  # allow dups, but expire old ones when I hit HISTSIZE
 setopt EXTENDED_HISTORY        # save timestamp and runtime information
 setopt SHARE_HISTORY           # import new commands from history file & append
