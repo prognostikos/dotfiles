1bbf65669e02585d8fae27b381fe5fdef94d9856
Matt Rohrer <matt@prognostikos.com>
2024-01-22 10:35:05 +0100

    Better handling of duplicate history entries
    https://registerspill.thorstenball.com/p/which-command-did-you-run-1731-days

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -6,3 +6,8 @@ HISTFILESIZE=99999
 setopt HIST_EXPIRE_DUPS_FIRST  # allow dups, but expire old ones when I hit HISTSIZE
 setopt EXTENDED_HISTORY        # save timestamp and runtime information
 setopt SHARE_HISTORY           # import new commands from history file & append
+setopt HIST_IGNORE_SPACE       # dont record an entry starting with a space
+setopt HIST_IGNORE_ALL_DUPS    # ddlete old recorded entry if new entry is a duplicate
+setopt HIST_IGNORE_DUPS        # dont record an entry that was just recorded again
+setopt HIST_EXPIRE_DUPS_FIRST  # expire duplicate entries first when trimming history
+setopt HIST_FIND_NO_DUPS       # do not display a line previously found
