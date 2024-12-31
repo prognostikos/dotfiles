f0148e40c96b22024433e26f307a6fc046928b82
Matt Rohrer <matt@prognostikos.com>
2024-12-31 07:56:48 +0100

    Resore last cursor position when opening files

--- config.symlink/nvim/init.lua
+++ config.symlink/nvim/init.lua
@@ -209,6 +209,17 @@ vim.api.nvim_create_autocmd('WinLeave', {
   end,
 })
 
+-- restore last cursor position
+vim.api.nvim_create_autocmd('BufReadPost', {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
