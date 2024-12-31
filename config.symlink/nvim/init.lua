df1a7b081b3751af8352c325f9860583b8025a22
Matt Rohrer <matt@prognostikos.com>
2024-12-31 13:37:14 +0100

    Add standardrb lsp

--- config.symlink/nvim/init.lua
+++ config.symlink/nvim/init.lua
@@ -669,6 +669,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         ruby_lsp = {},
+        standardrb = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
