1b56c6b9916b6c09ba7ac80ac8c6c3365702a7b4
Matt Rohrer <matt@prognostikos.com>
2024-12-31 16:34:34 +0100

    Allow openining in horizontal splits from telescope

--- config.symlink/nvim/init.lua
+++ config.symlink/nvim/init.lua
@@ -462,13 +462,17 @@ require('lazy').setup({
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
         -- pickers = {}
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-s>'] = require('telescope.actions').select_horizontal,
+            },
+            n = {
+              ['<C-s>'] = require('telescope.actions').select_horizontal,
+            }
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
