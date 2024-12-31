25569da5c1e8c3be8a7ebd37ee0cbb4d9bf43d1b
Matt Rohrer <matt@prognostikos.com>
2024-12-31 13:08:21 +0100

    add buffer completion

--- config.symlink/nvim/init.lua
+++ config.symlink/nvim/init.lua
@@ -809,6 +809,7 @@ require('lazy').setup({
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
     },
     config = function()
       -- See `:help cmp`
