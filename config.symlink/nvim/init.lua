c1e9589a442ca311fb20079893d342a67ce659e8
Matt Rohrer <matt@prognostikos.com>
2025-01-01 12:38:28 +0100

    Add additional config for autopairs

--- config.symlink/nvim/init.lua
+++ config.symlink/nvim/init.lua
@@ -295,7 +295,15 @@ require('lazy').setup({
   {
     'windwp/nvim-autopairs',
     event = 'InsertEnter',
-    config = true,
+    -- Optional dependency
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    config = function()
+      require('nvim-autopairs').setup {}
+      -- If you want to automatically add `(` after selecting a function or method
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
+      local cmp = require 'cmp'
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
   },
 
   {
