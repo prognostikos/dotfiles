047ccc7697f64b58f9306f34f6a401dd0b8f7ed0
Matt Rohrer <matt@prognostikos.com>
2025-01-02 10:01:27 +0100

    Install ruby treesitter plugin

--- config.symlink/nvim/init.lua
+++ config.symlink/nvim/init.lua
@@ -1025,7 +1025,20 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'ruby',
+        'query',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
