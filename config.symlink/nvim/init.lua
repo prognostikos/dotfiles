e140e4d44ca4e9962bcb5c04865b20cf6cff6b57
Matt Rohrer <matt@prognostikos.com>
2025-01-23 19:42:35 +0100

    Enable lsp rename

--- config.symlink/nvim/init.lua
+++ config.symlink/nvim/init.lua
@@ -813,7 +813,7 @@ require('lazy').setup({
 
           -- Rename the variable under your cursor.
           --  Most Language Servers support renaming across files, etc.
-          -- map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('<leader>rN', vim.lsp.buf.rename, '[R]e[n]ame')
 
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
