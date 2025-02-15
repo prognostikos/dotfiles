a08d266ea1d99dc8520d57e5b69f8bf052435e78
Matt Rohrer <matt@prognostikos.com>
2025-02-15 09:51:03 +0100

    Enable buffer completion for nvim-cmp

--- config.symlink/nvim/init.lua
+++ config.symlink/nvim/init.lua
@@ -1022,6 +1022,7 @@ require('lazy').setup({
             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
             group_index = 0,
           },
+          { name = 'buffer' },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
