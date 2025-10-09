9d3565d037a9743c752b2d0174874d0d947f9a7a
Matt Rohrer <matt@prognostikos.com>
2025-10-09 11:45:11 +0200

    Disable autocomplete in text files

--- config.symlink/nvim/init.lua
+++ config.symlink/nvim/init.lua
@@ -1200,5 +1200,14 @@ require('lazy').setup({
     },
   })
 
+-- Disable autocomplete in markdown, text, and gitcommit files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'text', 'gitcommit' },
+  group = vim.api.nvim_create_augroup('disable_cmp', { clear = true }),
+  callback = function()
+    require('cmp').setup.buffer({ enabled = false })
+  end
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
