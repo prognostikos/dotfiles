87d34dd5aa6e7cb3bba5785256dc25a33c8efb6d
Matt Rohrer <matt@prognostikos.com>
2025-10-17 16:12:42 +0200

    Only do this if we have `brew`

--- ruby-build/environment.zsh
+++ ruby-build/environment.zsh
@@ -1,1 +1,3 @@
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+if command -v brew > /dev/null; then
+  export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+fi
