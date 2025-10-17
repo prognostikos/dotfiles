fea0e489c6aba984d99762d3b9e8dace3a191fd6
Matt Rohrer <matt@prognostikos.com>
2025-10-17 16:16:09 +0200

    Not using vagrant that much

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,13 +1,8 @@
 local hostname="%m:"
 local username=''
 
-if [ $USER = vagrant ]; then
-  username="v@"
-  hostname="vm:"
-else
-  [ -z $SSH_TTY ]       && hostname=''
-  [ "$USER" != rohrer ] && username="%n@"
-fi
+[ -z $SSH_TTY ] && hostname=''
+[ "$USER" != rohrer ] && username="%n@"
 
 PROMPT='%{$fg[magenta]%}${username}${hostname}%{$fg[blue]%}%.%{$fg[cyan]%}$(git-cwd-info)%{$reset_color%}%# '
 
