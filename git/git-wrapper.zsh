b8faab2e37ae520c3448ab441ae3a1046354057f
Matt Rohrer <matt@prognostikos.com>
2025-10-24 17:28:23 +0200

    Switch to wrapping git itself

---
+++ git/git-wrapper.zsh
@@ -0,0 +1,10 @@
+git() {
+  if [ -n "${RUNNING_IN_DEVCONTAINER:-}" ]; then
+    if [ "$1" = "commit" ] || [ "$1" = "push" ]; then
+      echo "Error: git $1 is disabled in devcontainer environments" >&2
+      return 1
+    fi
+  fi
+
+  command git "$@"
+}
