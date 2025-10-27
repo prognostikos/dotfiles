5a27ca0be9e7cb082b3eeb0f6eaf8deb87c32dcf
Matt Rohrer <matt@prognostikos.com>
2025-10-27 15:23:04 +0100

    I only want to see the local branches

--- fzf/commands.zsh
+++ fzf/commands.zsh
@@ -78,13 +78,13 @@ fbr() {
 }
 
 # fbr - checkout git branch (including remote branches)
-fbr() {
-  local branches branch
-  branches=$(git branch --all | grep -v HEAD) &&
-  branch=$(echo "$branches" |
-           fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
-  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
-}
+# fbr() {
+#   local branches branch
+#   branches=$(git branch --all | grep -v HEAD) &&
+#   branch=$(echo "$branches" |
+#            fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+#   git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+# }
 
 # fco - checkout git branch/tag
 fco() {
