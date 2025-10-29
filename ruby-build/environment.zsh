a4f80167340a0ee9f43735c5a502ad1f60a0f642
Matt Rohrer <matt@prognostikos.com>
2025-10-29 12:36:21 +0100

    Disable for now

--- ruby-build/environment.zsh
+++ ruby-build/environment.zsh
@@ -1,3 +1,3 @@
 if command -v brew > /dev/null; then
-  export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+  # export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 fi
