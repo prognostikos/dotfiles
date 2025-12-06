4be6ba3c1928eac87b44dd09ce07a39d4bcc658e
Matt Rohrer <matt@prognostikos.com>
2025-12-06 12:08:23 +0100

    feat(fzf): tokyo night theme
    https://github.com/folke/tokyonight.nvim/blob/main/extras/fzf/tokyonight_night.sh

--- fzf/commands.zsh
+++ fzf/commands.zsh
@@ -9,6 +9,31 @@ fi
 # To apply the command to CTRL-T as well
 [[ -n "$FZF_DEFAULT_COMMAND" ]] && export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
+# colors
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --highlight-line \
+  --info=inline-right \
+  --ansi \
+  --layout=reverse \
+  --border=none \
+  --color=bg+:#283457 \
+  --color=bg:#16161e \
+  --color=border:#27a1b9 \
+  --color=fg:#c0caf5 \
+  --color=gutter:#16161e \
+  --color=header:#ff9e64 \
+  --color=hl+:#2ac3de \
+  --color=hl:#2ac3de \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#2ac3de \
+  --color=query:#c0caf5:regular \
+  --color=scrollbar:#27a1b9 \
+  --color=separator:#ff9e64 \
+  --color=spinner:#ff007c \
+"
+
 if fzf --zsh > /dev/null 2>&1; then
   source <(fzf --zsh)
 else
