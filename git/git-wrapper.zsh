8db48cc72b3537496876ef9c95fdee38f502ebf1
Matt Rohrer <matt@prognostikos.com>
2026-02-02 14:04:32 +0100

    feat: enable commit in devcontainers

--- git/git-wrapper.zsh
+++ git/git-wrapper.zsh
@@ -1,6 +1,6 @@
 git() {
   if [ -n "${RUNNING_IN_DEVCONTAINER:-}" ]; then
-    if [ "$1" = "commit" ] || [ "$1" = "push" ]; then
+    if [ "$1" = "push" ]; then
       echo "Error: git $1 is disabled in devcontainer environments" >&2
       return 1
     fi
