6a666aa2a52a6d4d7c3c3085272d9241d3284d0e
Matt Rohrer <matt@prognostikos.com>
2026-02-11 18:19:13 +0100

    fix: do not assume $HOME/.dotfiles

--- fzf/completion.sh
+++ fzf/completion.sh
@@ -5,8 +5,8 @@ if command -v fzf > /dev/null 2>&1; then
     source <(fzf --zsh)
   else
     # should be downloaded from github by script/setup
-    source ~/.dotfiles/fzf/key-bindings.zsh
-    source ~/.dotfiles/fzf/completion.zsh
+    source "$ZSH/fzf/key-bindings.zsh"
+    source "$ZSH/fzf/completion.zsh"
   fi
   bindkey '^P' fzf-history-widget
 fi
