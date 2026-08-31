df01d5ce39fd711ed4bd38436d29ed81aab9ae69
Matt Rohrer <matt@prognostikos.com>
2026-08-31 06:39:56 +0000

    fix: use version-matched fzf shell integration

--- fzf/completion.sh
+++ fzf/completion.sh
@@ -3,10 +3,22 @@
 if command -v fzf > /dev/null 2>&1; then
   if fzf --zsh > /dev/null 2>&1; then
     source <(fzf --zsh)
+  elif [[ -r /usr/share/doc/fzf/examples/key-bindings.zsh && -r /usr/share/doc/fzf/examples/completion.zsh ]]; then
+    source /usr/share/doc/fzf/examples/key-bindings.zsh
+    source /usr/share/doc/fzf/examples/completion.zsh
   else
-    # should be downloaded from github by script/setup
-    source "$ZSH/fzf/key-bindings.zsh"
-    source "$ZSH/fzf/completion.zsh"
+    fzf_version=$(fzf --version | cut -d " " -f 1)
+    fzf_cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/dotfiles/fzf/$fzf_version"
+
+    if [[ -r "$fzf_cache_dir/key-bindings.zsh" && -r "$fzf_cache_dir/completion.zsh" ]]; then
+      source "$fzf_cache_dir/key-bindings.zsh"
+      source "$fzf_cache_dir/completion.zsh"
+    fi
+
+    unset fzf_cache_dir fzf_version
   fi
-  bindkey '^P' fzf-history-widget
+
+  if (( ${+widgets[fzf-history-widget]} )); then
+    bindkey '^P' fzf-history-widget
+  fi
 fi
